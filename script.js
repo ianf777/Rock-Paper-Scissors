d6dc1a58649ac0d528efac01dc2ebbe5a8ff9074
ianf777 <ianwfuzzy@gmail.com>
2024-01-15 14:23:21 -0500

    comments added

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 
+//operation that generates a random, then determines if it's rock, paper, or scissors, from the numberic results
 function computerHand(min, max) {
   let randomizer = Math.floor(Math.random() * (max - min + 1)) + min;
   hand = "";
@@ -20,6 +21,7 @@ function computerHand(min, max) {
   return hand;
 }
 
+//Logic that represents a real life RPS match, winner is kept record of, and depending on the winner a message is displayed, score of player or computer goes up by 1 if they win
 function round(playerChoice, computersChoice) {
   /*DRAW CASE*/
   if (playerChoice === computersChoice) {
@@ -48,6 +50,7 @@ function round(playerChoice, computersChoice) {
   }
 }
 
+// function that plays a round, accepts two parameters representing the player and the computer's hands. the results are then dynamically inserted to the body of the page
 function playRound(playerChoice, computerHand) {
   round(playerChoice.toLowerCase(), computerHand(1, 3));
   const results = document.querySelector(".results");
@@ -59,6 +62,7 @@ function playRound(playerChoice, computerHand) {
   `;
 }
 
+//event listeners that run the functions on click, depending on which button is clicked, the player's input will be determiend as rock, paper, or scissors.
 rock.addEventListener("click", function () {
   playRound("rock", computerHand);
 });
